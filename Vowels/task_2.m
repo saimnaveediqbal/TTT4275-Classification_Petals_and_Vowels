5db7c7dbf9211c1039eb22d2c6b25984dcfdceb0
Saim Iqbal <saimni@stud.ntnu.no>
2023-04-24 15:10:30 +0200

    2a

--- Vowels/task_2.m
+++ Vowels/task_2.m
@@ -4,3 +4,8 @@ clc
 clear all
 [training_set, testing_set, vowel_code, talker_group_code, talker_number, vowel_classes] = prepare_data();
 %% (a) Finding GMM models
+for vowel = 1:12
+    vowel_set = training_set(vowel_code==vowel, :);
+    vowel_set = table2array(vowel_set(:, ["F1_ss" "F2_ss" "F3_ss"]))
+    GMM = fitgmdist(vowel_set, 2, 'RegularizationValue', 0.0001, 'CovarianceType', 'diagonal');
+end
